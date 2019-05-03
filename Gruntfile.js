3556b0b1c5c414ce7bf889459c067239cb9659e6
harriszhang <harriszhang@live.cn>
2019-05-03 22:58:34 +0800

    update

--- Gruntfile.js
+++ Gruntfile.js
@@ -5,7 +5,7 @@ module.exports = function (grunt) {
         pkg: grunt.file.readJSON('package.json'),
         uglify:{
             options: {
-                banner: '/*\n * <%= pkg.name %>\n * https://github.com/canos-oss/angularjs-canos\n * Version: <%= pkg.version %> - <%= grunt.template.today("yyyy-mm-dd") %>\n * License: Apache-2.0\n */\n'
+                banner: '/**\n * <%= pkg.name %>\n * @link https://github.com/canos-oss/angularjs-canos\n *\n * @version: <%= pkg.version %> - <%= grunt.template.today("yyyy-mm-dd") %>\n * @license: Apache-2.0\n */\n'
               },
               build: {
                 src: 'src/<%= pkg.name %>.js',
