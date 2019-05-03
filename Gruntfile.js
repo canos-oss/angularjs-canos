8305ec249eba5921a95e55c85fb864eabf23bc76
harriszhang <harriszhang@live.cn>
2019-05-03 12:25:22 +0800

    update

---
+++ Gruntfile.js
@@ -0,0 +1,20 @@
+module.exports = function (grunt) {
+    'use strict'
+
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        uglify:{
+            options: {
+                banner: '/*\n * <%= pkg.name %>\n * https://github.com/canos-oss/angularjs-canos\n * <%= grunt.template.today("yyyy-mm-dd") %>\n * License: Apache-2.0\n */\n'
+              },
+              build: {
+                src: 'src/<%= pkg.name %>.js',
+                dest: 'build/<%= pkg.name %>.min.js'
+              }  
+        }
+    })
+
+    grunt.loadNpmTasks('grunt-contrib-uglify')
+
+    grunt.registerTask('default', ['uglify'])
+}
---
+++ src/angularjs-canos.js
@@ -0,0 +1,36 @@
+﻿/**
+ * Canos 公共方法
+ */
+(function (window, angular) {
+
+    'use strict';
+
+    function ConvertorProvider() {
+        this.$get = function () {
+
+            var methods = {};
+
+            methods.toSelected = function (selections, key) {
+                if (selections == null || selections.length == 0) {
+                    return;
+                }
+
+                var items = [];
+                for (var i = 0; i < selections.length; i++) {
+                    if (!selections[i].selected) {
+                        continue;
+                    }
+                    items.push(selections[i][key]);
+                }
+
+                return items;
+            }
+
+            return methods;
+        };
+    }
+
+    angular.module('ngCanos', []).
+        provider('$convertor', [ConvertorProvider]);
+
+})(window, window.angular);
